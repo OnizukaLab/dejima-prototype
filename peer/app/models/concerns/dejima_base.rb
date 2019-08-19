a1362bb5bec9caba4d2de3853085f89db6c75d6b
Y-Wakuta <prsprivatestock@i.softbank.jp>
2019-08-19 20:18:29 +0900

    replace link model <-> dejima_table from hardcode to config file

--- peer/app/models/concerns/dejima_base.rb
+++ peer/app/models/concerns/dejima_base.rb
@@ -1,8 +1,21 @@
+require 'yaml'
+
 module DejimaBase
   extend ActiveSupport::Concern
 
-  included do
+  included do |base|
     self.dejima_tables = nil
+
+    config = YAML.load_file(ENV['CONFIG'])
+
+    views = []
+    config["peer_types"][Rails.application.config.dejima_peer_type.to_s]["base_table"][base.to_s]["dejima_table"].each do |dt|
+      view = {}
+      view[:table] = dt.constantize
+      view[:peers] = config["dejima_tables"][dt]["peers"].to_set
+      views << view
+    end
+    base.link_dejima_tables views
   end
 
   class_methods do
