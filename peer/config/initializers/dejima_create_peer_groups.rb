30c8f977a7d168b20e34519bbfcefc29ee6feb9d
Y-Wakuta <prsprivatestock@i.softbank.jp>
2019-09-22 07:32:32 -0400

    update README and makefile

--- peer/config/initializers/dejima_create_peer_groups.rb
+++ peer/config/initializers/dejima_create_peer_groups.rb
@@ -5,9 +5,8 @@ Rails.application.config.after_initialize do
     Rails.logger.info "Running as dejima peer type: #{Rails.application.config.dejima_peer_type}"
 
     config = YAML.load_file(ENV['CONFIG'])
-
-    config["peer_types"][Rails.application.config.dejima_peer_type.to_s]["base_table"].each do |base_table|
-      DejimaUtils.create_peer_groups(base_table.constantize.table_name)
+    config["peer_types"][Rails.application.config.dejima_peer_type.to_s]["base_table"].each do |bt|
+      DejimaUtils.create_peer_groups(bt.constantize.table_name)
     end
   end
 end
--- query.sh
+++
@@ -1,1 +0,0 @@
-curl http://localhost:81/dejima/create_user/first3/last3 | jq .
