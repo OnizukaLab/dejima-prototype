0f5666287e9a9adac2263f9cb941b5cb81dc5974
ekayim <k.ekayim@gmail.com>
2020-07-22 10:18:36 +0900

    simplify

--- proxy/execution.py
+++ proxy/execution.py
@@ -25,7 +25,7 @@ class Execution(object):
 
         # ----- connect to postgreSQL -----
         try:
-            db_conn = psycopg2.connect("connect_timeout=1 dbname=postgres user=dejima password=barfoo host={}-db port=5432".format(self.peer_name))
+            db_conn = psycopg2.connect("connect_timeout=1 dbname=postgres user=dejima password=barfoo host=Univ-db port=5432".format(self.peer_name))
         except Exception as e:
             msg = {"result": "Failed (cannot connect PostgreSQL)"}
             resp.body = json.dumps(msg)
