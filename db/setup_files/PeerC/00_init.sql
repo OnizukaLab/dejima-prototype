a5474f1e86b0f67b3b6d5ae3881e0771344afbad
ekayim <k.ekayim@gmail.com>
2021-05-19 09:12:04 +0900

    improve: remove bt_lineage from init.sql

--- db/setup_files/PeerC/00_init.sql
+++ db/setup_files/PeerC/00_init.sql
@@ -15,12 +15,3 @@ select
 	left(md5((i+1)::text), 5) as col2,
 	left(md5((i+2)::text), 5) as col3 
 from generate_series(1,10000) as i;
-
-CREATE TABLE BT_LINEAGE (
-		ID		INT PRIMARY KEY,
-        LINEAGE VARCHAR(80)
-	);
-
-\echo 'LOADING bt_lineage'
-INSERT INTO bt_lineage (id, lineage)
-select i as id, '<PeerA,bt,' || i::text || '>' from generate_series(1,10000) as i;
