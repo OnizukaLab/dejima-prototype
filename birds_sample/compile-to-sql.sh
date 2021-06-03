543a3c5c44dca3ee761853f1409707cfea30d968
ekayim <k.ekayim@gmail.com>
2021-06-04 02:20:18 +0900

    Other: add birds sample

---
+++ birds_sample/compile-to-sql.sh
@@ -0,0 +1,4 @@
+birds -f peera_a1.dl -o peera_a1.sql --dejima -b dejima_update.sh;
+birds -f mediator_a1.dl -o mediator_a1.sql --dejima -b dejima_update.sh;
+birds -f mediator_a2.dl -o mediator_a2.sql --dejima -b dejima_update.sh;
+birds -f peerb_a2.dl -o peerb_a2.sql --dejima -b dejima_update.sh;
---
+++ birds_sample/dejima_update.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+result=$(curl -s -X POST -H "Content-Type: application/json" $DEJIMA_EXECUTION_ENDPOINT -d "$1")
+if  [ "$result" = "true" ];  then
+    echo "true"
+else 
+    echo $result
+fi
